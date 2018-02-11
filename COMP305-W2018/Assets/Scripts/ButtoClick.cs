e4912588d05e6ecc3eba0889737be505f3ab08d6
Pragnesh Patel <patelpragnesh85@gmail.com>
2018-02-10 19:34:57 -0500

    Lab-2

---
+++ COMP305-W2018/Assets/Scripts/ButtoClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtoClick : MonoBehaviour {
+
+	public GameObject cBoy;
+
+	public Sprite newSprite;
+	private SpriteRenderer sRend;
+
+	// Use this for initialization
+	void Start () {
+
+		sRend = cBoy.GetComponent<SpriteRenderer>();
+
+	}
+
+	void OnMouseDown()
+	{
+		sRend.sprite = newSprite;
+
+	}
+}
